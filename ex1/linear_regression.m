2b14df58110c50a103f1c518ba6dc2ede911dee6
khushhallchandra <kcm00000@gmail.com>
2016-01-26 22:49:53 +0530

    Added linear regression

--- ex1/linear_regression.m
+++ ex1/linear_regression.m
@@ -22,3 +22,8 @@ function [f,g] = linear_regression(theta, X,y)
   %        computed gradient in 'g'.
   
 %%% YOUR CODE HERE %%%
+
+for i=1:size(X,2)
+    f = f + .5 * (theta' * X(:,i) - y(i))^2;
+    g = g + (theta' * X(:,i) - y(i)) * X(:,i);
+end  
